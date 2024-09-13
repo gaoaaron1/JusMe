32e0ac9e721bf3e5c68755d097baef5eb9fb15e5
Aaron Gao <gaoaaron1@gmail.com>
2024-09-13 10:21:30 -0600

    Added home page

---
+++ frontend/src/Components/Item/Item.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Item.css'
+
+function Item() {
+    return (
+        <div className='item'>
+            <img src="" alt="" />
+        </div>
+    )
+}
+
+export default Item
