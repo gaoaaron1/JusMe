9ebe70b41948d090ea4fa00cdfa91326b35abdfe
Aaron Gao <gaoaaron1@gmail.com>
2024-09-14 23:52:11 -0600

    Completed homepage

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
 
 function Shop() {
@@ -12,6 +13,7 @@ function Shop() {
             <Popular />
             <Offers />
             <NewCollections />
+            <NewsLetter />
         </div>
     )
 }
