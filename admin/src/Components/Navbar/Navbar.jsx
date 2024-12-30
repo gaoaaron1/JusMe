647b3f9e99ae5758c735a203e07bd7dc5ee31742
Aaron Gao <gaoaaron1@gmail.com>
2024-12-30 00:14:01 -0700

    Added add product feature integrated with database

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
-import navProfile from '../../assets/nav-profile.svg'
+import navlogo from '../../assets/JusMe-Logo.svg'
+import navProfile from '../../assets/aaron-profile.png'
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <img src={navlogo} alt="" className="nav-logo" />
-            <img src={navProfile} className="navprofile" alt="" />
+            <img src={navProfile} className="nav-profile" alt="Profile" />
         </div>
     )
 }
